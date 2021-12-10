b3e147a4b525ba686d81b4af9a789fa75e2cb254
James Pham <james.pham2@ontariotechu.net>
2021-12-10 15:59:40 -0500

    JoyStick

---
+++ Assets/Scripts/joystickmovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joystickmovement : MonoBehaviour
+{
+    public Joystick joystick;
+    public CharacterController player;
+    Vector3 movementDirection;
+    public float gravity = 15.0f;//Gravity intensity
+    public float speed;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float hori = joystick.Horizontal;
+        float vert = joystick.Vertical;
+
+        movementDirection = new Vector3(hori, 0, vert);
+        movementDirection = transform.TransformDirection(movementDirection);
+
+        movementDirection *= speed;
+
+        movementDirection.y -= gravity;
+
+        player.Move(movementDirection * Time.deltaTime);
+
+        
+
+    }
+}

829c4a9737d44279496220d00576ea2b50f3d006
James Pham <james.pham2@ontariotechu.net>
2021-12-13 15:16:32 -0500

    public void JoinRoom() Error

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,57 +1,58 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//using Phonton.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-//public class Launcher : MonoBehaviour
-//{
-//    public InputField createInput;
-//    public InputField joinInput;
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public InputField createInput;
+    public InputField joinInput;
+    public Text error;
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        Debug.Log("Connecting");
-//        PhotonNetwork.ConnectUsingSettings();
-//    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+        PhotonNetwork.JoinLobby();
+    }
 
-//    public override void OnConnectedToMaster()
-//    {
-//        Debug.Log("Connected");
-//        PhotonNetwork.JoinLobby();
-//    }
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Main Lobby");
+    }
 
-//    public override void OnJoinedLobby()
-//    {
-//        Debug.Log("Joined Main Lobby");
-//    }
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(createInput.text))
+                return;
+            PhotonNetwork.CreateRoom(createInput.text);
+    }
 
-//    public void CreateRoom()
-//    {
-//        if (string.IsNullOrEmpty(createInput.text))
-//            return;
-//        PhotonNetwork.CreateRoom(createInput.text);
-//    }
+    //    public void JoinRoom()
+    //    {
+    //        PhontonNetwork.JoinRoom(joinInput.text);
+    //    }
 
-//    public void JoinRoom()
-//    {
-//        PhontonNetwork.JoinRoom(joinInput.text);
-//    }
+    public override void OnJoinedRoom()
+    {
 
-//    public override void OnJoinedRoom()
-//    {
+    }
 
-//    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Error creating room!");
+    }
 
-//    public override void OnCreateRoomFailed(short returnCode, string message)
-//    {
-//        Debug.Log("Error creating room!");
-//    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//    }
-//}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
